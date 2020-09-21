c712fbb7e4af93f78c9bab81546abe6a5e3206a0
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-22 00:25:28 +0600

    Add files via upload

---
+++ ReservedTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alphasoft.Models.RMSTable
+{
+    public class ReservedTable
+    {
+        public int Id { get; set; }
+        public int AvailableTable { get; set; }
+        public DateTime BookingStartTime { get; set; }
+        public DateTime BookingEndTime { get; set; }
+        public int ReservedTableNumber { get; set; }
+        public int TableId { get; set; }
+        public Table Table { get; set; }
+    }
+}
---
+++ TableDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alphasoft.Models.RMSTable
+{
+    public class TableDetails
+    {
+        public int Id { get; set; }
+     
+        public int OrderSeat { get; set; }
+        public int TotalChir { get; set; }
+     
+        public int AvaiableSeat { get; set; }
+
+        // AvaiableSeat=TotalChair - OrderSeat//
+        public DateTime ChoseTimeStart { get; set; }
+        public DateTime ChoseTimeEnd { get; set; }
+
+        public int TableId { get; set; }
+        public Table Tables { get; set; }
+    }
+}
