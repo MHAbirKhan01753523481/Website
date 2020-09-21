78bd5fdc11029df4d560926055009d338df25f30
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-22 00:21:31 +0600

    Add files via upload

---
+++ ReservationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alphasoft.Enum
+{
+    public enum ReservationType
+    {
+    
+        FamilyProgramParty = 101,
+         BirthDayParty = 102,
+         FriendParty = 103,
+         OfficailParty = 104,
+         CorporateParty = 105,
+         OfficalParty = 106,
+         ConferenceParty = 107,
+         WeddingParty = 108,
+    }
+}
